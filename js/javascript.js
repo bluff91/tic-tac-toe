25451a9991e0fb8f789b438bfca950252d29321a
bluff <bluffyx91@gmail.com>
2022-08-14 18:04:01 +0300

    Completed; added what was left of the logic, and encapsulated everything into 2 modules. No global variables left

--- js/javascript.js
+++ js/javascript.js
@@ -1,92 +1,128 @@
-const X_CLASS= 'x';
-const O_CLASS = 'o';
-let circleTurn;
-const WINNING_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-];
 
-const selectBox = document.querySelectorAll('[data-box]');
-const board = document.getElementById('board');
-const winningMessage = document.querySelector('[data-winning-message-text]');
-const winningMessageElement = document.getElementById('winningMessage');
-const restartBtn = document.getElementById('restartBtn');
+//GamePlay
 
-startGame();
+var gamePlay = (function(){
 
-restartBtn.addEventListener('click', startGame);
+    const WINNING_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
 
-function startGame(){
-    circleTurn=false;
-    selectBox.forEach(box => {
-        box.classList.remove(X_CLASS);
-        box.classList.remove(O_CLASS);
-        box.removeEventListener('click', handleClick)
-        box.addEventListener('click', handleClick, {once: true})
-    })
-    setBoardHover();
-    winningMessageElement.classList.remove('show');
-}
-
-function handleClick(e) {
-    console.log('clicked');
-    const box = e.target
-    const currentClass = circleTurn ? O_CLASS : X_CLASS;
-    placeMark(box, currentClass);
-    if (checkWin(currentClass)) {
-        endGame(false);
-    } else if (isDraw()){
-        endGame(true)
-    } else {
-        swapTurns();
-        setBoardHover();    
+    function handleClick(e)  {
+        'use strict';
+        const box = e.target
+        const currentClass = displayController.circleTurn ? displayController.O_CLASS : displayController.X_CLASS;
+        placeMark(box, currentClass);
+        if (checkWin(currentClass)) {
+            endGame(false);
+        } else if (isdraw()){
+             endGame(true)
+        } else {
+            swapTurns();
+            setBoardHover();    
+        }
     }
-}
 
-function isDraw() {
-    return [...selectBox].every(box => {
-        return box.classList.contains(X_CLASS) || 
-                box.classList.contains(O_CLASS);
-    })
-}
-
-function endGame(draw){
-     if (draw) {
-        winningMessage.innerText = 'Draw !'
-     } else {
-        winningMessage.innerText = `${circleTurn ? "Os ": "Xs "}WIN!!!`; 
-     }
-     winningMessageElement.classList.add('show');
-}
-
-function placeMark(box, currentClass) {
-    box.classList.add(currentClass);
-}
-
-function swapTurns() {
-    circleTurn=!circleTurn;
-}
-
-function setBoardHover() {
-    board.classList.remove(X_CLASS);
-    board.classList.remove(O_CLASS);
-    if(circleTurn) {
-        board.classList.add(O_CLASS);
-    } else {
-        board.classList.add(X_CLASS);
-    }
-}
-
-function checkWin(currentClass) {
-    return WINNING_COMBINATIONS.some(combination => {
-        return combination.every(index => {
-            return selectBox[index].classList.contains(currentClass);
+    function startGame(){
+        gamePlay.circleTurn=false;
+        displayController.selectBox().forEach(box => {
+            box.classList.remove(displayController.X_CLASS);
+            box.classList.remove(displayController.O_CLASS);
+            box.removeEventListener('click', handleClick)
+            box.addEventListener('click', handleClick, {once: true})
         })
-    })
-}
+        setBoardHover();
+        displayController.winningMessageElement().classList.remove('show');
+    }
+
+    function isdraw() {
+        return [...displayController.selectBox()].every(box => {
+            return box.classList.contains(displayController.X_CLASS) || 
+                    box.classList.contains(displayController.O_CLASS);
+        })
+    }
+
+    function endGame(draw){
+        if (draw) {
+           displayController.winningMessage().innerText = 'Draw !'
+        } else {
+           displayController.winningMessage().innerText = `${displayController.circleTurn ? "Os ": "Xs "}WIN!!!`; 
+        }
+        displayController.winningMessageElement().classList.add('show');
+    }
+
+   function placeMark(box, currentClass) {
+        box.classList.add(currentClass);
+    }
+
+    function swapTurns() {
+        displayController.circleTurn=!displayController.circleTurn;
+    }
+
+    function setBoardHover() {
+        displayController.board().classList.remove(displayController.X_CLASS);
+        displayController.board().classList.remove(displayController.O_CLASS);
+        if(displayController.circleTurn) {
+            displayController.board().classList.add(displayController.O_CLASS);
+        } else {
+            displayController.board().classList.add(displayController.X_CLASS);
+        }   
+    }
+
+    function checkWin(currentClass) {
+        return WINNING_COMBINATIONS.some(combination => {
+            return combination.every(index => {
+                return displayController.selectBox()[index].classList.contains(currentClass);
+            })
+        })
+    }
+    return {
+        startGame,
+    }
+
+})();
+
+//displayController
+
+var displayController = (function() {
+    
+    const X_CLASS= 'x';
+    const O_CLASS = 'o';
+    let circleTurn;
+
+    const selectBox = document.querySelectorAll('[data-box]');
+    const board = document.getElementById('board');
+    const winningMessage = document.querySelector('[data-winning-message-text]');
+    const winningMessageElement = document.getElementById('winningMessage');
+    const restartBtn = document.getElementById('restartBtn');
+    restartBtn.addEventListener('click', gamePlay.startGame);
+
+    return {
+        selectBox:function() {return selectBox},
+        board:function() {return board},
+        winningMessage:function() {return winningMessage},
+        winningMessageElement:function() {return winningMessageElement},
+        X_CLASS,
+        O_CLASS,
+        circleTurn
+    }
+})();
+
+gamePlay.startGame();
+
+
+
+
+
+
+
+
+
+
+
